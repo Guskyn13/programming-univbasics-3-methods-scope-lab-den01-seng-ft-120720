3f21871e409b78cde22b59b0b04e91171eb6aa65
Nathan K Gusky <n.gusky88@gmail.com>
2020-11-29 09:15:28 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -16,7 +16,7 @@ end
 describe "all_phrases" do
   it "puts out all of the previous catch phrases" do
     expect{mario}.to output(/It's-a me, Mario!\n/).to_stdout
-    expect{status}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
+    expect{toadstool}.to output(/Thank You Mario! But Our Princess Is In Another Castle!\n/).to_stdout
     expect{link}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
 
   end
